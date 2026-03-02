63ffd84f996aae6672df210e44f86d97c228c21c
Alex ValiDev <abubekirov.alex@gmail.com>
2026-03-02 22:21:53 +0100

    if isset type client added active default. client form select type client remove border

--- components/c-client-choice.php
+++ components/c-client-choice.php
@@ -11,8 +11,8 @@
                 </div>
             </div>
             <div class="c-client-choice__col o-col-6@md o-col-push-3@md o-col-8@sm o-col-4@xs">
-                <div class="c-client-choice__wrapper-type-client">
-                    <div class="c-client-choice__type-client js-client-choice__type-client <?php echo $type_client === 'particular' || !isset($post_id) ? 'is-active' : '' ?>" data-id="particular">
+                <div class="c-client-choice__wrapper-type-client">                    
+                    <div class="c-client-choice__type-client js-client-choice__type-client <?php echo $type_client === 'particular' || !isset($post_id) || !isset($type_client) ? 'is-active' : '' ?>" data-id="particular">
                         <p>Cliente Particular</p>
                     </div>
                     <div class="c-client-choice__type-client js-client-choice__type-client <?php echo $type_client === 'profesional' ? 'is-active' : '' ?>" data-id="profesional">
